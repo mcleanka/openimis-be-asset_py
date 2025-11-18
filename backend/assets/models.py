496d76e5b7e019a1f08abe2c77aad4be29123577
mcleanka <mcleankasambala@gmail.com>
2025-11-18 20:47:45 +0200

    added delete

--- backend/assets/models.py
+++ backend/assets/models.py
@@ -148,30 +148,30 @@ class User(models.Model):
 
         super().save(*args, **kwargs)
 
-    # @property
-    # def can_delete(self):
-    #     """
-    #     Check if user can be safely deleted.
-    #     """
-    #     asset_count = self.assigned_assets.count()
+    @property
+    def can_delete(self):
+        """
+        Check if user can be safely deleted.
+        """
+        asset_count = self.assigned_assets.count()
 
-    #     if asset_count > 0:
-    #         return False, f"User has {asset_count} assigned asset(s)", asset_count
+        if asset_count > 0:
+            return False, f"User has {asset_count} assigned asset(s)", asset_count
 
-    #     return True, "User can be deleted", 0
+        return True, "User can be deleted", 0
 
-    # def delete(self, *args, **kwargs):
-    #     """Override delete to handle business rules"""
-    #     # Check if user has assigned assets
-    #     if self.assigned_assets.exists():
-    #         raise ValidationError(
-    #             f"Cannot delete user '{self.name}' because they have assets assigned. "
-    #             "Unassign all assets first."
-    #         )
+    def delete(self, *args, **kwargs):
+        """Override delete to handle business rules"""
+        # Check if user has assigned assets
+        if self.assigned_assets.exists():
+            raise ValidationError(
+                f"Cannot delete user '{self.name}' because they have assets assigned. "
+                "Unassign all assets first."
+            )
 
-    #     # If no assets assigned, proceed with deletion
-    #     # The SET_NULL will handle the role and region fields
-    #     super().delete(*args, **kwargs)
+        # If no assets assigned, proceed with deletion
+        # The SET_NULL will handle the role and region fields
+        super().delete(*args, **kwargs)
 
 
 class Asset(models.Model):
